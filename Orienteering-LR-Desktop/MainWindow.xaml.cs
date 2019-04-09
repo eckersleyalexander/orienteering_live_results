0acac81cb808ede15ae4f8613d53f90958eb20b9
Sam <sam@mooreof.me>
2019-04-09 16:12:08 +1000

    Add missing dependency.

--- Orienteering-LR-Desktop/MainWindow.xaml.cs
+++ Orienteering-LR-Desktop/MainWindow.xaml.cs
@@ -48,13 +48,13 @@ namespace Orienteering_LR_Desktop
                 db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
                
                 var count = db.SaveChanges();
-                Console.WriteLine("{0} records saved to database", count);
+                Debug.WriteLine("{0} records saved to database", count);
 
-                Console.WriteLine();
-                Console.WriteLine("All blogs in database:");
+                Debug.WriteLine("");
+                Debug.WriteLine("All blogs in database:");
                 foreach (var blog in db.Blogs)
                 {
-                    Console.WriteLine(" - {0}", blog.Url);
+                    Debug.WriteLine(" - {0}", blog.Url);
                 }
             }
         }
