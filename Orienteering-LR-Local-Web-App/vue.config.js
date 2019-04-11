6e7e00976d7fafae7a652c921dcc36ef10f9fa6e
u5561865-drc <u5561865@anu.edu.au>
2019-04-11 15:02:43 +1000

    Fixed build configuration. Closes #44. Vue app is now built to the appropriate desktop app build directory.

--- Orienteering-LR-Local-Web-App/vue.config.js
+++ Orienteering-LR-Local-Web-App/vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    // currently builds only to the debug folder
-    outputDir: "../Orienteering-LR-Desktop/bin/Debug/vue_app"
-        // process.env.NODE_ENV === 'production'
-    // ? '../Orienteering-LR-Desktop/bin/release/vue_app' // production build path
-    // : '../Orienteering-LR-Desktop/bin/Debug/vue_app' // debug build path
-}
+    outputDir:
+        process.env.NODE_ENV === "production"
+            ? "../Orienteering-LR-Desktop/bin/release/vue_app" // production build path
+            : "../Orienteering-LR-Desktop/bin/Debug/vue_app" // debug build path
+};
