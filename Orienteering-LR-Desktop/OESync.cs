79b0fc3e7f30acab8c436c531f9898a5988aeaa4
Winston Kint <u6497984@anu.edu.au>
2019-08-21 20:17:25 +1000

    added timer begin work on db writing

--- Orienteering-LR-Desktop/OESync.cs
+++ Orienteering-LR-Desktop/OESync.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Data;
 using System.IO;
 
@@ -11,14 +11,17 @@ namespace Orienteering_LR_Desktop
     class OESync
     {
         public string OEEventPath;
-        private int SyncTime;
+        private int SyncTime; // in seconds
         private bool Running;
+        private Timer SyncFunc;
+        
 
         public OESync(string path)
         {
             OEEventPath = path;
             SyncTime = 30;
             Running = false;
+            SyncFunc = null;
         }
 
         public OESync(string path, int timer)
@@ -26,6 +29,7 @@ namespace Orienteering_LR_Desktop
             OEEventPath = path;
             SyncTime = timer;
             Running = false;
+            SyncFunc = null;
         }
 
         private DataSet ReadOEDb()
@@ -46,17 +50,47 @@ namespace Orienteering_LR_Desktop
             DataSet OEdb = ReadOEDb();
 
             // for each of our tables: read the appropriate data from the DataSet
-            // then write to our db record by record
+            // drop the current tables
+            // then write all the records to our db
+
+            if (!OEdb.Tables.Contains("Telin"))
+            {
+                return;
+            }
+
+            using (var context = new Database.CompetitorContext())
+            {
+                //context.Competitors.RemoveRange(context.Competitors);
+
+            }
+
+            // competitor
+
+            // club
+
+            // team
+
+            // comptimes - times incomplete
+
+            // raceclass
+
+            
         }
 
         public void StartSync()
         {
             // begin syncing on timer
+            SyncFunc = new Timer(e => OESyncDataNow(), null, TimeSpan.Zero, TimeSpan.FromSeconds(SyncTime));
         }
 
         public void StopSync()
         {
             // stop syncing
+            if(SyncFunc != null)
+            {
+                SyncFunc.Dispose();
+                SyncFunc = null;
+            }
         }
     }
 }
