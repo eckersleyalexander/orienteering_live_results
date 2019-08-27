dfcef87e5002383453ceb367222ae841952047f1
Winston Kint <u6497984@anu.edu.au>
2019-08-27 15:01:10 +1000

    Revert "can send commands over websocket"
    This reverts commit 7fc7ef42ab6c5a0cb9e6b2e5a5e144d5516bea09.

--- Orienteering-LR-Desktop/API/ControlSocket.cs
+++ Orienteering-LR-Desktop/API/ControlSocket.cs
@@ -10,13 +10,7 @@ namespace Orienteering_LR_Desktop.API
        
     public class ControlSocket : WebSocketModule
     {
-        public class CommandMsg
-        {
-            public string Command;
-            public string Body;
-        }
-
-        public class SetScreenCommand
+        public class SetScreenClassesMsg
         {
             public string ScreenId;
             public List<int> RaceClassIds;
@@ -41,42 +35,7 @@ namespace Orienteering_LR_Desktop.API
 
         protected override Task OnMessageReceivedAsync(IWebSocketContext context, byte[] buffer, IWebSocketReceiveResult result)
         {
-            try
-            {
-                CommandMsg command = JsonConvert.DeserializeObject<CommandMsg>(System.Text.Encoding.Default.GetString(buffer));
-                string reply;
-
-                switch (command.Command)
-                {
-                    case "SetScreen":
-                        SetScreenCommand body = JsonConvert.DeserializeObject<SetScreenCommand>(command.Body);
-                        reply = "Screen " + body.ScreenId + " set to classes: ";
-                        List<string> added = new List<string>();
-                        foreach (Database.RaceClass raceClass in new Database.Query().GetClasses())
-                        {
-                            if (body.RaceClassIds.Contains(raceClass.RaceClassId))
-                            {
-                                added.Add(raceClass.Name);
-                            }
-                        }
-                        reply += string.Join(",", added);
-                        break;
-                    case "GetClasses":
-                        reply = JsonConvert.SerializeObject(new Database.Query().GetAllClassInfo());
-                        break;
-                    case "GetScreens":
-                        reply = "GetScreens";
-                        break;
-                    default:
-                        reply = "Unrecognized Command";
-                        break;
-                }
-                SendAsync(context, reply);
-            }
-            catch (JsonReaderException)
-            {
-                SendAsync(context, "Invalid Message Format");
-            }
+            SetScreenClassesMsg command = JsonConvert.DeserializeObject<SetScreenClassesMsg>(System.Text.Encoding.Default.GetString(buffer));
             return Task.WhenAll();
         }
     }
